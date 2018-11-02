810fd03237a3bd247dae1f385f64cc2685514c21
Stefan Svinciak <stefan.svinciak@gmail.com>
2018-11-02 16:03:59 +0100

    created settings for heroku deploy

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
-from flask_restful import Api
-from app.Hello import Hello
+from flask import Flask
 
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+app = Flask(__name__)
 
-# Route
-api.add_resource(Hello, '/Hello')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    app.run()
--- run.py
+++ run.py
@@ -1,18 +1,12 @@
 from flask import Flask
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
+app = Flask(__name__)
 
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
 
-    from Model import db
-    db.init_app(app)
-
-    return app
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 
 if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
+    app.run()
