f6bffb2a67c386ec6fa57fc19f9b6ef32f88ad81
Michaela Bamburová <michaela.bamburova@gmail.com>
2018-11-21 09:52:59 +0100

    UserAnswerService.get_answer_by_id + API call

--- src/views/UserAnswerView.py
+++ src/views/UserAnswerView.py
@@ -1,32 +1,28 @@
 from flask import json, Response, Blueprint
-
-from ..models.UserAnswerModel import UserAnswerModel, UserAnswerSchema
+from src.services import UserAnswerService
 
 # CONFIG
-user_answers_blueprint = Blueprint('user_answers', __name__, url_prefix='/api/v1/user_answers')
-
-user_answers_schema = UserAnswerSchema(many=True)
-user_answer_schema = UserAnswerSchema()
+user_answers_blueprint = Blueprint('user_answers', __name__, url_prefix='/api/v1/answer')
 
 
 # ROUTES
-@user_answers_blueprint.route('/', methods=['GET'])
-def get_all_answers():
-    user_answers = UserAnswerModel.query.all()
-    response = user_answers_schema.dump(user_answers)
-    return custom_response(response, 200)
+@user_answers_blueprint.route('/<int:user_id>/<int:question_id>/<int:answer_id>', methods=['GET'])
+def get_answer_by_id(user_id, question_id, answer_id):
+    if UserAnswerService.already_answered(question_id) is not None:
+        return custom_response(
+            {"error": "Bad request - question is already answered"},
+            400
+        )
 
-
-@user_answers_blueprint.route('/<int:answer_id>', methods=['GET'])
-def get_answer_by_id(answer_id):
-    answer = UserAnswerModel.get_answer_by_id(answer_id)
-    response = user_answer_schema.dump(answer)
-    return custom_response(response, 200)
+    return custom_response(
+        UserAnswerService.save_answer(user_id, question_id, answer_id),
+        200
+    )
 
 
 def custom_response(res, status_code):
     return Response(
         mimetype="application/json",
-        response=json.dumps(res.data),
+        response=json.dumps(res),
         status=status_code
     )
