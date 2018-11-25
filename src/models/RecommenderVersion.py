eade91541656e7dfc1d02c57ff029b5637536884
ssvinciak <stefan.svinciak@gmail.com>
2018-11-25 18:48:25 +0100

    added user creation api call (#2)

---
+++ src/models/RecommenderVersion.py
@@ -0,0 +1,8 @@
+import enum
+
+
+class RecommenderVersion(enum.Enum):
+    RR = 1
+    NR = 2
+    RN = 3
+    NN = 4
--- src/views/UserView.py
+++ src/views/UserView.py
@@ -1,19 +1,34 @@
 from flask import json, Response, Blueprint
 
+from src.models import db
+from src.models.RecommenderVersion import RecommenderVersion
 from ..models.UserModel import UserModel, UserSchema
 
 # CONFIG
-users_blueprint = Blueprint('users', __name__, url_prefix='/api/v1/users')
+users_blueprint = Blueprint('users', __name__, url_prefix='/api/v1')
 
 users_schema = UserSchema(many=True)
 user_schema = UserSchema()
 
 
-# ROUTES
-@users_blueprint.route('/', methods=['GET'])
+@users_blueprint.route('/all-users', methods=['GET'])
 def get_all_users():
     users = UserModel.query.all()
     response = users_schema.dump(users)
+    return custom_response(response, 200)
+
+
+@users_blueprint.route('/new-user', methods=['GET'])
+def get_new_user():
+    user = UserModel({
+        "version": choose_recommender_version()
+    })
+
+    db.session.add(user)
+    db.session.commit()
+    db.session.refresh(user)
+
+    response = user_schema.dump(user)
     return custom_response(response, 200)
 
 
@@ -23,3 +38,8 @@ def custom_response(res, status_code):
         response=json.dumps(res.data),
         status=status_code
     )
+
+
+def choose_recommender_version():
+    # TODO: add logic for choosing rec version
+    return RecommenderVersion.RR.value
