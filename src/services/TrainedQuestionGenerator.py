0d23b369110038a5077cfd046082e68290fa6b5b
michaela <michaela.bamburova@cogvio.com>
2018-12-02 23:47:55 +0100

    small improvements in generating questions

--- src/services/TrainedQuestionGenerator.py
+++ src/services/TrainedQuestionGenerator.py
@@ -31,27 +31,29 @@ def get_user_answered_questions(user_id):
 
 def generate_first_question():
     sorted_idioms = get_sorted_idioms_with_frequency()
+    median = int(round(len(sorted_idioms) / 2))
 
-    return sorted_idioms[int(round(len(sorted_idioms)/2))]
+    return sorted_idioms[random.randint(median - 15, median + 15)]
 
 
 def generate_question_based_on_success_rate(answered_questions):
     sorted_idioms = list(get_sorted_idioms_with_frequency())
+    idioms_count = len(sorted_idioms)
 
     last_answered_question = answered_questions[0]
     position = get_position_of_last_question(sorted_idioms, last_answered_question)
 
     rate = success_rate(answered_questions)
     if rate > 0.9:
-        return sorted_idioms[get_new_question_position(position + 61)]
+        return sorted_idioms[get_new_question_position(position + 61, idioms_count)]
     elif rate > 0.8:
-        return sorted_idioms[get_new_question_position(position + 21)]
+        return sorted_idioms[get_new_question_position(position + 21, idioms_count)]
     elif rate > 0.6:
-        return sorted_idioms[get_new_question_position(position - 21)]
+        return sorted_idioms[get_new_question_position(position - 21, idioms_count)]
     elif rate > 0.4:
-        return sorted_idioms[get_new_question_position(position - 31)]
+        return sorted_idioms[get_new_question_position(position - 31, idioms_count)]
     else:
-        return sorted_idioms[get_new_question_position(position - 61)]
+        return sorted_idioms[get_new_question_position(position - 61, idioms_count)]
 
 
 def success_rate(answered_questions):
@@ -73,18 +75,24 @@ def get_position_of_last_question(sorted_idioms, last_answered_question):
         else:
             count = count + 1
 
-    return None
+    return None  # should never happen
 
 
-def get_new_question_position(new_position):
+def get_new_question_position(new_position, idioms_count):
     min_range = new_position - 10
     max_range = new_position + 10
 
-    if min_range < 0:
+    if min_range < 0 or max_range < 0:
         min_range = 0
+        max_range = 20
 
-    if max_range > 1041:
-        max_range = 1041
+    if min_range > idioms_count or max_range > idioms_count:
+        min_range = median_from_questions()
+        max_range = idioms_count
 
     return random.randint(min_range, max_range)
 
+
+def median_from_questions():
+    sorted_idioms = get_sorted_idioms_with_frequency()
+    return int(round(len(sorted_idioms) / 2))
